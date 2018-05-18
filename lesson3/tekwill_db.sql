0b8df493ef02886a3dbca6c30129d17f5f7113fb
Stefan <Stefan@Stefans-MacBook-Air.local>
2018-05-18 20:47:55 +0300

    upload la db.sql

---
+++ lesson3/tekwill_db.sql
@@ -0,0 +1,14 @@
+--select * from AD_EXAM_TYPE 
+
+/*select department_id, location_id
+from DEPARTMENTS*/
+
+--describe DEPARTMENTS 
+SELECT salary * 1.1 --inmulteste salariu cu 10%, daca era 1.2 ->20%
+from EMPLOYEES
+
+SELECT salary 
+from EMPLOYEES
+
+select 10+(15-5*2)/7 
+from dual --tabel de test
